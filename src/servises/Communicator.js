6cbed4dc0c276770b582830962f900a46a6780ee
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-18 23:53:28 +0200

    feat: communicator

---
+++ src/servises/Communicator.js
@@ -0,0 +1,11 @@
+export default class Communicator {
+  constructor(programState) {
+    this.programState = programState;
+  }
+
+  WELCOME = () => process.stdout.write(`Welcome to the File Manager, ${this.programState.userName}!\n\n`);
+  YOU_ARE_IN = () => process.stdout.write(`You are currently in ${this.programState.currentDirectory}\n\n`);
+  GOODBYE = () => process.stdout.write(`Thank you for using File Manager, ${this.programState.userName}, goodbye!\n\n`);
+
+  ERROR = (error) => process.stdout.write(`${error.message || error.toString()}\n\n`);
+}
